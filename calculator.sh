f551966e49815747a44ed7e66d70ac1ba6067a21
shiru1104 <shirisharockin1997@gmail.com>
2023-08-11 17:55:15 +0000

    testing cherrypick

--- calculator.sh
+++ calculator.sh
@@ -1,1 +1,11 @@
 x=a+b
+<<<<<<< HEAD
+=======
+
+
+making changes in dev branch 
+seeing the example of cherry picking
+
+
+making second change in dev branch
+>>>>>>> a2b5ce2 (second commit in dev)
